45d2e041775db87f329dd409e46c2d7cbfdfdcad
Felix Yuen <142155456+felfelyuen@users.noreply.github.com>
2025-04-11 20:29:35 +0800

    Update Top_Student.v

--- felix code/Top_Student.v
+++ felix code/Top_Student.v
@@ -24,35 +24,35 @@ module Top_Student(
     input basys_clock,
     input [4:0] pb,
     input [2:0] sw,
-    output [7:0] JB
+    output [7:0] JA
 );
 
-wire [15:0] oled_data; 
-wire fb; wire [12:0] pi; wire sendp; wire samplep; wire SLOWCLK_6_25MHZ; 
-
-flexible_clock_divider clock6_25MHZ (basys_clock, 4'b0111, SLOWCLK_6_25MHZ);
-
+    wire [15:0] oled_data_two; 
+    wire SLOWCLK_6_25MHZ; 
+    wire fb_two; wire [12:0] pi_two; wire sendp_two; wire samplep_two; wire SLOWCLK_6_25MHZ; 
+//flexible_clock_divider clock6_25MHZ (basys_clock, 4'b0111, SLOWCLK_6_25MHZ);
+    //we are using JA for the maze (or anything)
 Oled_Display oleddd (
     .clk(SLOWCLK_6_25MHZ), 
     .reset(0), 
-    .frame_begin(fb), 
-    .sending_pixels(sendp), 
-    .sample_pixel(samplep), 
-    .pixel_index(pi), 
+    .frame_begin(fb_two), 
+    .sending_pixels(sendp_two), 
+    .sample_pixel(samplep_two), 
+    .pixel_index(pi_two), 
     .pixel_data(oled_data), 
-    .cs(JB[0]), 
-    .sdin(JB[1]), 
-    .sclk(JB[3]), 
-    .d_cn(JB[4]), 
-    .resn(JB[5]), 
-    .vccen(JB[6]),
-    .pmoden(JB[7]));
+    .cs(JA[0]), 
+    .sdin(JA[1]), 
+    .sclk(JA[3]), 
+    .d_cn(JA[4]), 
+    .resn(JA[5]), 
+    .vccen(JA[6]),
+    .pmoden(JA[7]));
   
-wire [9:0] x; wire [6:0] y;
-coord_system coordinates (pi, x, y);
+    wire [6:0] x_two; wire [5:0] y_two;
+    coord_system coordinates (pi_two, x_two, y_two);
 wire [2:0] wire_to_cut; wire curr_colour;
 //curr_colour from isaac, wire_to_cut to ryan
 
-maze mazee (basys_clock, pb, x, y, sw, curr_colour, oled_data, wire_to_cut);
+maze mazee (basys_clock, pb, x_two, y_two, sw, curr_colour, oled_data_two, wire_to_cut);
 
 endmodule
