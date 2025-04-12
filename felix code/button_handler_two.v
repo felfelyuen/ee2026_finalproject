901f1b563c60cf6cfa13326c6d3fc945d5411989
Isaaclks7 <141604983+Isaaclks7@users.noreply.github.com>
2025-04-12 16:18:41 +0800

    Update button_handler_two.v

--- felix code/button_handler_two.v
+++ felix code/button_handler_two.v
@@ -42,8 +42,7 @@ module button_handler_two(
     always @(posedge CLK) begin 
         if (RESET == 0) begin
             next_count = 181;
-        end else if (game_pause == 0) begin
-        end else
+        end else if (game_pause == 0) begin end else 
         if (next_count == 255 & CTRLbtn) begin
             next_count = begin_spot;
         end else
