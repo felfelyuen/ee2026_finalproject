dad6062554ba31df3a22c27b634d9d6828c9ae5b
Isaaclks7 <141604983+Isaaclks7@users.noreply.github.com>
2025-04-14 01:19:33 +0800

    Update and rename coord_system.v to convert_to_xy.v

--- convert_to_xy.v
+++ convert_to_xy.v
@@ -3,9 +3,9 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 03/11/2025 07:50:46 PM
+// Create Date: 13.03.2025 22:23:45
 // Design Name: 
-// Module Name: coord_system
+// Module Name: convert_to_xy
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module coord_system(
+module convert_to_xy(
     input [12:0] pixel_index,
     output [6:0] x,
     output [5:0] y
